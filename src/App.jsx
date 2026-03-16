94a4a78457d3b32b07678ed4f3c997bac4f70f5c
sowjanya-gangisetty <sowji.gsetty@gmail.com>
2026-03-16 22:53:49 +0100

    mobileMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
 import styles from './app.module.css'
 import Header from './components/Header'
-import Categories from './components/Categories'
+//import Categories from './components/Categories'
 import CategoriesContainer from './components/CategoriesContainer'
 import  MainCategory from './components/MainCategory'
 
 
 function App() {
   
-
   return (
     <>
       <Header/>
@@ -16,7 +15,6 @@ function App() {
         <MainCategory/>
         <CategoriesContainer />
       </div>
-    
     </>
   )
 }
--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,12 +1,24 @@
 import styles from './navigation.module.css'
+import { useState } from 'react';
+import { RxHamburgerMenu } from "react-icons/rx";
+
 
 const  categories = ["Men", "Women", "Kids", "Cosmetics" , "Accessories"]
 
+
 const Navigation = () => {
+
+    const [mobileMenu, setMobileMenu] = useState(false) 
+
+    const showMobileMenu = () => {
+        setMobileMenu(!mobileMenu)
+    }
+
     return(
 
         <nav className={styles.navigation}>
-            <div className={styles.navContainer}>
+            <RxHamburgerMenu  onClick={showMobileMenu} className={styles.mobileIcon} size={32} />
+            <div className={`${styles.navContainer}  ${mobileMenu ? styles.active : ''}`}>
                 <div className={styles.menuItems}>{categories.map((category, index) => <span key={index} className={styles.category}>{category}</span>)}</div>
             </div>
         </nav>
